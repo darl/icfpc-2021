19b0b0667132bd56a4bf6a89ad3b9df7596bb5c9
McSim1993 <McSim1993@gmail.com>
2021-07-09 17:05:26 +0300

    checkFits

--- build.sbt
+++ build.sbt
@@ -5,3 +5,5 @@ version := "0.1"
 scalaVersion := "2.13.6"
 
 idePackagePrefix := Some("icfpc21.classified")
+
+libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.9" % "test"
--- src/main/scala/optimizer/Scorer.scala
+++ src/main/scala/optimizer/Scorer.scala
@@ -3,9 +3,18 @@ package optimizer
 
 import icfpc21.classified.model.{Figure, Hole}
 
+import java.awt.Polygon
+
 object Scorer {
   def scoreLikes(figure: Figure, eps: Int): Double = ???
 
-  def checkFits(figure: Figure, hole: Hole): Boolean = ???
+  def checkFits(figure: Figure, hole: Hole): Boolean = {
+    val poly = new Polygon(
+      hole.points.map(_.x).toArray,
+      hole.points.map(_.y).toArray,
+      hole.points.size
+    )
+    figure.vertices.forall(p => poly.contains(p.x, p.y))
+  }
 
 }
