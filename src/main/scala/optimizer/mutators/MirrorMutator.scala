f695484eb42eda4cac412ac3045dd2a729c75e59
Harand97 <madnix7997@gmail.com>
2021-07-09 22:44:12 +0300

    Better mirror

--- src/main/scala/optimizer/mutators/MirrorMutator.scala
+++ src/main/scala/optimizer/mutators/MirrorMutator.scala
@@ -8,9 +8,12 @@ import scala.util.Random
 
 object MirrorMutator extends Mutator {
   override def mutate(figure: Figure, hole: Hole, speed: Double): Figure = {
-    if (Random.nextBoolean())
-      figure.copy(vertices = figure.vertices.map(v => v.copy(x = -v.x)))
-    else
-      figure.copy(vertices = figure.vertices.map(v => v.copy(y = -v.y)))
+    if (Random.nextBoolean()) {
+      val xMiddle = (figure.vertices.map(_.x).max + figure.vertices.map(_.x).min) / 2
+      figure.copy(vertices = figure.vertices.map(v => v.copy(x = 2 * xMiddle - v.x)))
+    } else {
+      val yMiddle = (figure.vertices.map(_.y).max + figure.vertices.map(_.y).min) / 2
+      figure.copy(vertices = figure.vertices.map(v => v.copy(y = 2 * yMiddle - v.y)))
+    }
   }
 }
