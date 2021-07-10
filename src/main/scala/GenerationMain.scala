607fd6e8f165703bdc7b90107027b0dc80b76055
Vladislav Dolbilov <darl@yandex-team.ru>
2021-07-10 13:33:24 +0300

    progress on #4

--- src/main/scala/GenerationMain.scala
+++ src/main/scala/GenerationMain.scala
@@ -6,58 +6,13 @@ import visualization.Visualizer
 import model._
 
 object GenerationMain extends App {
-  val problemId = 5 // args(1).toInt
+  val problemId = 4 // args(1).toInt
   val client = new PosesClient.Live("https://poses.live", Key.value)
 
   val checkpoints = Map(
-    // dislikes: 634.0
-    4 -> Solution(
-      List(
-        Vector(95, 11),
-        Vector(94, 27),
-        Vector(94, 37),
-        Vector(89, 7),
-        Vector(89, 32),
-        Vector(85, 53),
-        Vector(89, 22),
-        Vector(74, 32),
-        Vector(74, 37),
-        Vector(74, 52),
-        Vector(74, 57),
-        Vector(71, 67),
-        Vector(69, 47),
-        Vector(69, 62),
-        Vector(50, 26),
-        Vector(64, 21),
-        Vector(64, 32),
-        Vector(64, 47),
-        Vector(64, 62),
-        Vector(65, 83),
-        Vector(59, 52),
-        Vector(59, 57),
-        Vector(53, 67),
-        Vector(49, 22),
-        Vector(49, 52),
-        Vector(49, 57),
-        Vector(35, 15),
-        Vector(44, 32),
-        Vector(44, 37),
-        Vector(44, 47),
-        Vector(44, 62),
-        Vector(44, 85),
-        Vector(39, 47),
-        Vector(39, 62),
-        Vector(5, 50),
-        Vector(34, 52),
-        Vector(34, 57),
-        Vector(31, 70),
-        Vector(24, 32),
-        Vector(24, 37),
-        Vector(24, 52),
-        Vector(6, 8),
-        Vector(14, 37)
-      )
-    )
+    // dislikes: 397.0
+    4 -> Solution(List(Vector(12,11), Vector(6,27), Vector(5,37), Vector(6,6), Vector(11,32), Vector(13,53), Vector(10,21), Vector(26,32), Vector(26,37), Vector(25,52), Vector(25,57), Vector(29,67), Vector(31,47), Vector(31,62), Vector(49,25), Vector(35,19), Vector(34,30), Vector(36,47), Vector(36,63), Vector(35,85), Vector(41,52), Vector(41,57), Vector(47,71), Vector(51,22), Vector(51,52), Vector(51,57), Vector(65,15), Vector(56,32), Vector(56,37), Vector(56,47), Vector(56,63), Vector(58,89), Vector(61,47), Vector(61,62), Vector(95,50), Vector(66,52), Vector(67,57), Vector(69,70), Vector(76,32), Vector(77,37), Vector(77,52), Vector(95,5), Vector(86,34)))
+
   )
 
   val problem = client.getProblem(problemId)
@@ -66,10 +21,10 @@ object GenerationMain extends App {
     case Some(checkpoint) => problem.copy(figure = problem.figure.copy(vertices = checkpoint.vertices))
     case None             => problem
   }
-  val visualizer = Visualizer(problem)
+  val visualizer = Visualizer(problem2)
   val t = new Thread(() => visualizer.show())
   t.start()
-  val solution = new GenerationalSolver(visualizer).solve(problem)
+  val solution = new GenerationalSolver(visualizer).solve(problem2)
   t.join()
 
   println(solution)
