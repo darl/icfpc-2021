fc9485eef908a0c92641e94c20448886ce54cb50
Vladislav Dolbilov <darl@yandex-team.ru>
2021-07-11 19:18:48 +0300

    strip tokens

--- src/main/scala/Tokens.scala
+++ src/main/scala/Tokens.scala
@@ -3,6 +3,6 @@ package icfpc21.classified
 import java.nio.file.{Files, Path}
 
 object Tokens {
-  val value = Files.readString(Path.of(".token"))
-  val session = Files.readString(Path.of(".session"))
+  val value = Files.readString(Path.of(".token")).strip()
+  val session = Files.readString(Path.of(".session")).strip()
 }
